ff4e70f41b40691a009f76700c04ffd057f5a0af
arachne1102 <s1284123@gmail.com>
2024-10-23 18:56:12 +0900

    commit

--- lib/screens/main/main_screen.dart
+++ lib/screens/main/main_screen.dart
@@ -2,15 +2,8 @@ import 'package:daelim_project/common/scaffold/app_scaffold.dart';
 import 'package:daelim_project/routes/app_screen.dart';
 import 'package:flutter/material.dart';
 
-class MainScreen extends StatefulWidget {
+class MainScreen extends StatelessWidget {
   const MainScreen({super.key});
-
-  @override
-  State<MainScreen> createState() => _MainScreenState();
-}
-
-class _MainScreenState extends State<MainScreen> {
-  int _selectedIndex = 0;
 
   @override
   Widget build(BuildContext context) {
